e8b9fc41c7242edf59faecaf96551ad5006a3d3d
trndav <farsiteguide@gmail.com>
2023-08-23 20:04:37 +0200

    Add stock result partial.

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,16 @@
 class StocksController < ApplicationController
     def search
-        @stock = Stock.new_lookup(params[:stock])        
-        render "users/my_portfolio"        
+        if params[:stock].present?
+            @stock = Stock.new_lookup(params[:stock])   
+            if @stock     
+                render "users/my_portfolio"
+            else
+                flash[:alert] = "Symbol you entered is not valid." 
+                redirect_to my_portfolio_path
+            end
+        else
+            flash[:alert] = "Search term must not be empty."
+            redirect_to my_portfolio_path
+        end       
     end
 end
