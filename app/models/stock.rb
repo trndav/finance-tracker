cd00731e5e4cbb8132d1bbc18f29eb891888c7c8
trndav <farsiteguide@gmail.com>
2023-08-24 12:04:37 +0200

    Stock look up functionality

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -6,8 +6,8 @@ class Stock < ApplicationRecord
         endpoint: 'https://cloud.iexapis.com/v1')
         begin
             new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
-        rescue => exception    
-            return nil        
+        rescue => exception
+            return nil
         end
     end
 end
