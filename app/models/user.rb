bd75ba922100a43e70693a5e1ff71bbe86592422
trndav <farsiteguide@gmail.com>
2023-09-03 11:24:58 +0200

    Fixed error, :stocks instead of :users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :user_stocks
-  has_many :users, through: :user_stocks
+  has_many :stocks, through: :user_stocks
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
