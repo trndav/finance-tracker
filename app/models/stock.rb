86b2bb1710e055c53432bcd4f8cdc59238f302ae
trndav <farsiteguide@gmail.com>
2023-09-04 20:15:15 +0200

    Stock listing view, add to list not working

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -7,12 +7,16 @@ class Stock < ApplicationRecord
     def self.new_lookup(ticker_symbol)        
     client = IEX::Api::Client.new(
         publishable_token: Rails.application.credentials.iex[:api_key],
-        secret_token:  Rails.application.credentials.iex[:api_secret], 
+        secret_token: Rails.application.credentials.iex[:api_secret], 
         endpoint: 'https://cloud.iexapis.com/v1')
         begin
             new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
         rescue => exception
             return nil
         end
+    end
+
+    def self.check_db(ticker_symbol)
+        where(ticker: ticker_symbol).first
     end
 end
