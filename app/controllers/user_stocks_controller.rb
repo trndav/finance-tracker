7df708c5925e6a337028714cdcfc210014568139
trndav <farsiteguide@gmail.com>
2023-09-05 13:19:52 +0200

    Remove stock from user stock in browser

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -12,4 +12,12 @@ class UserStocksController < ApplicationController
     def index
         @user_stocks = current_user.user_stocks # Fetch user's stocks from the database
       end
+
+    def destroy
+        stock = Stock.find(params[:id])
+        user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+        user_stock.destroy
+        flash[:notice] = "#{stock.ticker} was successfully removed from portfolio"
+        redirect_to my_portfolio_path
+    end
 end
